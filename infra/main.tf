fe07fd3f01a0ef6ecd0a1210a46aa11d52bcd243
Martin Stöcker <martin.stoecker99@gmail.com>
2023-03-26 14:02:05 +0200

    adding ci tf

---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "openra"
+  location = "westeurope"
+}
+
+resource "azurerm_container_group" "container_group" {
+  name                = "openra"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  os_type = "Linux"
+
+  ip_address_type = "Public"
+  dns_name_label  = "openra"
+
+  container {
+    name   = "openra"
+    image  = "registry.reindruecken.de/openra/redalert-server:latest"
+    cpu    = "1.0"
+    memory = "1.0"
+
+    ports {
+      port     = 1234
+      protocol = "TCP"
+    }
+  }
+}
+
+output "ip_address" {
+  value = azurerm_container_group.container_group.ip_address
+}
+
